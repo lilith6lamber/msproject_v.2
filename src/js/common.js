f7e76944a97ab72afc97419beab3f504455b9661
Lilith Lamber <winter.rituel@gmail.com>
2021-09-08 19:22:13 +0300

    load.screen promise to avoid unexpected behavior

--- src/js/common.js
+++ src/js/common.js
@@ -9,6 +9,8 @@ import tabs from "./modules/tabs";
 import { userLogin, logout, changeElemsOnAuth } from "./modules/auth";
 import { toggleForm } from "./modules/form";
 
+const wait = (delay = 0) =>
+    new Promise(resolve => setTimeout(resolve, delay));
 
 const setVisible = (selector, visible) => {
     selector = document.getElementById(selector);
@@ -17,15 +19,21 @@ const setVisible = (selector, visible) => {
         document.documentElement.style.overflowY = 'hidden';
     } else {
         selector.classList.remove("visible");
+        document.body.classList.add("visible");
         document.documentElement.style.overflowY = 'visible';
     }
 }
 
 setVisible('loadingScreen', true);
 
+document.addEventListener('DOMContentLoaded', () =>
+    wait(1000).then(() => {
+        setVisible('loadingScreen', false);
+    }));
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    setTimeout(setVisible, 1000, 'loadingScreen', false);
+/*     setTimeout(setVisible, 1000, 'loadingScreen', false); */
 
     let lazyLoadInstance = new LazyLoad({});
     changeElemsOnAuth();
